547bf06209bb70ca6c5ac4262b46cda2574f8126
JARDAA1 <jaroslav.antos@seznam.cz>
2026-01-01 10:51:43 +0100

    Remove: Odstranit mapa/_layout.tsx - může způsobovat React error 310

--- app/mapa/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function MapaLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
